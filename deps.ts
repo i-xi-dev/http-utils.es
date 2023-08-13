8759c29d0f3596b655f2a5ddd4715e72c2a4871d
i-xi-dev <naoyuki.isono@gmail.com>
2023-08-14 01:12:32 +0900

    2.1.7

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { StringUtils } from "https://raw.githubusercontent.com/i-xi-dev/str.es/2.0.0/mod.ts";
+export { StringUtils } from "https://raw.githubusercontent.com/i-xi-dev/str.es/2.0.1/mod.ts";
