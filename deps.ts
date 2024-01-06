18770a79768a24b589584cfb05f912e832f9b580
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-07 00:34:20 +0900

    2.1.18

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export * as StringEx from "https://raw.githubusercontent.com/i-xi-dev/string.es/3.6.0/mod.ts";
+export * as StringEx from "https://raw.githubusercontent.com/i-xi-dev/string.es/4.0.2/mod.ts";
