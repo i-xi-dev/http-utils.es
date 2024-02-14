e1eb8d0638634581c099a88e886db58486b21382
i-xi-dev <naoyuki.isono@gmail.com>
2024-02-14 20:36:36 +0900

    2.1.24

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export {
   CodePoint,
   StringEx,
-} from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.3/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.4/mod.ts";
