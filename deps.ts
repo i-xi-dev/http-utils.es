60549d893ed03e1db1cdcf79850d46edcfe2ffb4
i-xi-dev <naoyuki.isono@gmail.com>
2024-06-17 01:49:54 +0900

    2.1.27

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/7.0.0/mod.ts";
-export { type CodePointRange } from "https://raw.githubusercontent.com/i-xi-dev/unicode.es/1.0.0-beta.2/mod.ts";
+export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/7.0.1/mod.ts";
+export { type CodePointRange } from "https://raw.githubusercontent.com/i-xi-dev/unicode.es/1.0.1/mod.ts";
